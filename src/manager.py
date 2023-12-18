1199e4ae2ab81190148307c039b8dc7523d3436f
jan-wun <janwunderlich01@gmail.com>
2023-12-18 08:50:17 +0100

    system for saving and loading data

--- src/manager.py
+++ src/manager.py
@@ -1,35 +1,86 @@
-import pygame
+import pickle
+import os
 
 
-class AudioManager:
-    """
-    Manages audio-related functionalities.
-    """
-    def play_sound(self):
+class SaveLoadSystem:
+    def __init__(self, file_extension, save_folder):
         """
-        Plays a sound effect.
-        """
-        pass
+        Initializes a SaveLoadSystem instance.
 
-    def play_music(self):
+        Args:
+            file_extension (str): The file extension to be used for saved files.
+            save_folder (str): The folder where saved files will be stored.
         """
-        Plays background music.
-        """
-        pass
+        self.file_extension = file_extension
+        self.save_folder = save_folder
 
+    def save_data(self, data, name):
+        """
+        Saves data to a file.
 
-class ScoreManager:
-    """
-    Manages the game's score.
-    """
-    def update_score(self, distance):
+        Args:
+            data: The data to be saved.
+            name (str): The name of the file (excluding the extension).
         """
-        Updates the game score based on the distance covered.
-        """
-        pass
+        data_file = open(f"{self.save_folder}/{name}{self.file_extension}", "wb")
+        pickle.dump(data, data_file)
 
-    def display_score(self):
+    def load_data(self, name):
         """
-        Displays the current score.
+        Loads data from a file.
+
+        Args:
+            name (str): The name of the file (excluding the extension).
+
+        Returns:
+            The loaded data.
         """
-        pass
+        data_file = open(f"{self.save_folder}/{name}{self.file_extension}", "rb")
+        data = pickle.load(data_file)
+        return data
+
+    def check_for_file(self, name):
+        """
+        Checks if a file with the given name exists in the save folder.
+
+        Parameters:
+            name (str): The name of the file (excluding the extension).
+
+        Returns:
+            True if the file exists, False otherwise.
+        """
+        return os.path.exists(f"{self.save_folder}/{name}{self.file_extension}")
+
+    def load_game_data(self, files_to_load, default_data):
+        """
+        Loads game data from files, providing default values if a file is missing.
+
+        Parameters:
+            files_to_load (list): A list of file names to load data from.
+            default_data (list): A list of default values corresponding to each file.
+
+        Returns:
+            A tuple of loaded data or a single variable.
+        """
+        variables = []
+        for index, file in enumerate(files_to_load):
+            if self.check_for_file(file):
+                variables.append(self.load_data(file))
+            else:
+                variables.append(default_data[index])
+
+        if len(variables) > 1:
+            return tuple(variables)
+        else:
+            return variables[0]
+
+    def save_game_data(self, data_to_save, file_names):
+        """
+        Saves game data to files.
+
+        Parameters:
+            data_to_save (list): A list of data to be saved.
+            file_names (list): A list of file names to save data into.
+        """
+        for index, file in enumerate(data_to_save):
+            self.save_data(file, file_names[index])
